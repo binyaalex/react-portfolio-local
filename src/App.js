bb32b9061ef60aa53a977d3f161db7d6277939f7
unknown <binyaalex@gmail.com>
2022-03-30 18:03:05 +0300

    add reset.css

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Home from './components/Home'
 import Projects from './components/Projects'
 import Contacts from './components/Contacts'
 import logo from './logo.svg';
+import './reset.css';
 import './App.css';
 
 function App() {
